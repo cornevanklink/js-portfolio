44b5b656d4d3c4395a10ad3211e927697448ce33
Corné van Klink <cvklink@gmail.com>
2022-08-09 15:55:25 +0200

    Finish JS Caesar Cipher

--- FCC/fcc_caesars_cipher.js
+++ FCC/fcc_caesars_cipher.js
@@ -1,29 +1,48 @@
 /* Caesars cipher
 
-One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount. A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. 
+In a shift cipher the meanings of the letters are shifted by some set amount. 
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. 
+Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
 
-Write a function which takes a ROT13 encoded string as input and returns a decoded string. All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+Write a function which takes a ROT13 encoded string as input and returns a decoded string. 
+All letters will be uppercase. 
+Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
 
 */
 
 function rot13(str) {
   var decoded = "";
-  for (i = 0; i<str.length;i++){
-    var charcode = str.charCodeAt(i);
-    if(str[i] == "a"){
-        console.log("The charcode for " + str[i] +" at position" + i +  "is " +charcode);
+  var shift = -13;
+  var i = 0;
+
+  for (i = 0; i < str.length; i++) {
+    var echarcode = str.charCodeAt(i);
+
+    console.log(
+      "The charcode for " + str[i] + " at position " + i + " is " + echarcode
+    );
+
+    if (
+      echarcode != 33 &&
+      echarcode != 63 &&
+      echarcode != 46 &&
+      echarcode != 32
+    ) {
+      var dcharcode = echarcode + shift;
+      if (dcharcode < 65) {
+        dcharcode = 90 - (64 - dcharcode);
+      }
+      console.log(dcharcode);
+    } else {
+      var dcharcode = echarcode;
     }
-    if(str[i] !== " "){
-        var charcode = charcode + shift;
-        var char = String.fromCharCode(charcode);
-    }
-    else{
-        var char = " ";
-    }
-    
+    var char = String.fromCharCode(dcharcode);
+
     decoded += char;
-}
-    return decoded;
   }
-  
-  rot13("SERR PBQR PNZC");
+  console.log(decoded);
+  return decoded;
+}
+
+rot13("SERR YBIR?");
