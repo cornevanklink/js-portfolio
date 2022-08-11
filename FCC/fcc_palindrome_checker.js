e3683b6db91f4bddaadb553f0eb67150e76a9ddf
Corné van Klink <cvklink@gmail.com>
2022-08-11 15:50:58 +0200

    Finish Palindrome Checker

--- FCC/fcc_palindrome_checker.js
+++ FCC/fcc_palindrome_checker.js
@@ -1,25 +1,29 @@
-/*
-Return true if the given string is a palindrome. Otherwise, return false.
-A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
-Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
-We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
-We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
+function palindrome(str) {
+  // Cleaning up the string: removing all non-alphanumeric characters
+  var raw_input = str.replace(/[\W_]/g, "");
 
-*/
+  // Cleaning up the string: changing all letters to lower case
+  var raw_input = raw_input.toLowerCase();
 
+  // Changing string to array
+  var input = raw_input.split("");
 
-function palindrome_checker(str) {
-  x
+  // Calculating how many steps are needed to check for palindrome
+  var steps = Math.floor(input.length / 2);
+
+  var start = 0;
+  var end = input.length - 1;
+
+  for (steps; steps > 0; steps--) {
+    if (input[start] == input[end]) {
+      start++;
+      end--;
+    } else {
+      return false;
+    }
+  }
+
   return true;
 }
 
-
-
-
-/**
- * 1. Clean up string str (remove non-alphanumerical characters)
- * 2. Check if str.length is an odd number
- * 3. Turn str into array
- * 4. 
- * 
- */
+palindrome("never odd or even");
