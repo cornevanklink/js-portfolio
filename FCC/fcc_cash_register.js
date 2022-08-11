a1e3528f3e54bf646e90eb16bb1b5f2a40c11845
Corné van Klink <cvklink@gmail.com>
2022-08-11 20:34:20 +0200

    Started on FCC Cash Register

--- FCC/fcc_cash_register.js
+++ FCC/fcc_cash_register.js
@@ -37,9 +37,41 @@ See below for an example of a cash-in-drawer array:
 
 */
 
+var result = {
+  "status": "",
+  "change": []
+}
+
 function checkCashRegister(price, cash, cid) {
-    var change;
-    return change;
-  }
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  if(price == cash){
+    result.status = "CLOSED";
+
+    return result;
+  }
+
+  var change = cash - price;
+  console.log(change);
+
+  for(const x of cid){
+    console.log(x);
+  }
+
+
+
+  return change;
+  
+  
+}
+
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
