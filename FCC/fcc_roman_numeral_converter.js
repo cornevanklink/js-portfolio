34dc9c361d11eea548e652394c881a6fed7aac71
Corné van Klink <cvklink@gmail.com>
2022-08-11 15:56:57 +0200

    Started work on FCC Roman Numeral Converter

--- FCC/fcc_roman_numeral_converter.js
+++ FCC/fcc_roman_numeral_converter.js
@@ -2,7 +2,6 @@
 Convert the given number into a roman numeral.
 All roman numerals answers should be provided in upper-case.
 
-
 Roman Numerals:
 I = 1
 V = 5
@@ -15,17 +14,21 @@ M = 1000
 */
 
 function convertToRoman(num) {
-    // for > 1000
+  // for > 1000
+  var str = "";
+  for (num; num >= 1000; num -= 1000) {
+    console.log("Run")
+    str += "M";
+  }
 
-    // for 
-    
-    // if > 1000
+  // for
 
-    
-    // if > 500
+  // if > 1000
 
+  // if > 500
 
-    return num;
-   }
-   
-   convertToRoman(36);
+  console.log(str);
+  return num;
+}
+
+convertToRoman(1001);
